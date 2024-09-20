7520b357bfc59a194a80ec64cce68542a077030f
ganesh dusa <dusaganesh2@gmail.com>
2024-09-20 23:28:48 +0530

    sumof two numbers added

---
+++ login/src/login/Sum.java
@@ -0,0 +1,13 @@
+package login;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int sum = a + b;
+		System.out.println("addtion of two:" + sum);
+
+	}
+
+}
